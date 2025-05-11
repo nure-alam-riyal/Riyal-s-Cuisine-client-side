f139030698722897b4149169fdb320ab7682c2b0
nurealamriyal <nurealamriyal@gmail.com>
2025-05-11 18:08:01 +0600

    finish

--- src/Components/FoodCard.jsx
+++ src/Components/FoodCard.jsx
@@ -15,16 +15,31 @@ const FoodCard = ({ food }) => {
                     src={foodPhoto}
                     alt={foodName}/>
             </figure>
-            <div className="card-body flex">
-               <div className='flex-1'>
+            <div className="card-body flex ">
+               
                  <h2 className="card-title">Food Name:{foodName}</h2>
                 <p>Food Category:{foodCategory}</p>
                 <div className='flex justify-between'><p>Purchesed:{purchaseCount}</p> <p>Quantity:{foodQuantity}</p></div>
-               </div>
-                <div className="card-actions justify-end bottom">
+               
+                <div className="card-actions flex-col justify-end  ">
                     <Link to={`/food/${_id}`}><button className="btn btn-primary">Details</button></Link>
                 </div>
             </div>
+  {/* <div className="card h-full flex flex-col justify-between">
+  <div className="card-body flex flex-col gap-3">
+    <h2 className="card-title">Food Name: {foodName}</h2>
+    <p>Food Category: {foodCategory}</p>
+    <div className="flex justify-between">
+      <p>Purchased: {purchaseCount}</p>
+      <p>Quantity: {foodQuantity}</p>
+    </div>
+  </div>
+  <div className="card-actions p-4 pt-0">
+    <Link to={`/food/${_id}`} className="w-full">
+      <button className="btn btn-primary w-full">Details</button>
+    </Link>
+  </div>
+</div> */}
            </Fade>
         </div>
     );
